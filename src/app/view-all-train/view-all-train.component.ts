5e42634835732922d4f5937d774fdfb019555d85
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-15 11:00:33 +0530

    new

--- src/app/view-all-train/view-all-train.component.ts
+++ src/app/view-all-train/view-all-train.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-all-train',
@@ -7,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllTrainComponent implements OnInit {
 
-  constructor() { }
-name="Abin"
+  constructor(private myapi:ApiService) { 
+    this.fetchData()
+  }
+
+  fetchData=()=>{
+    this.myapi.viewData().subscribe(
+      (data)=>{
+        this.Data=data
+      }
+    )
+  }
+  status=true
+Data:any={}
   ngOnInit(): void {
   }
 
